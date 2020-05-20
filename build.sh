4406e0503eba44d8e9a558f0d774f2ba5498d674
Michael Mikhailov <yohanson@users.noreply.github.com>
2020-05-20 21:13:43 +0700

    Fix cursor hotspots and size inconsistency

--- build.sh
+++ build.sh
@@ -1,42 +1,94 @@
 #!/bin/bash
 
+set -e
+
+function get_inkscape_opts() {
+    if inkscape -V | grep -qF 'Inkscape 0'; then
+        echo '-z -e'
+    else
+        echo '-o'
+    fi
+}
+
+function get_frame() {
+    svg=$1
+    basename ${svg%.*}
+}
+
+function get_cursor() {
+    frame=$1
+    if [[ "$frame" =~ -[0-9]+$ ]]; then
+        echo ${frame%-*}
+    else
+        echo $frame
+    fi
+}
+
 function create {
+    BUILD="$(dirname "$SRC")/build"
+    CONFIGS="$BUILD/config"
+    DIST="$(dirname "$SRC")/dist"
+	OUTPUT="$DIST/cursors"
+	ALIASES="$SRC/cursorList"
+    HOTPOINTS="$SRC/hotpoints"
+    inkscape_opts=$(get_inkscape_opts)
+
+    mkdir -p "$CONFIGS"
+    mkdir -p "$OUTPUT"
+
 	cd "$SRC"
-	mkdir -p x1 x1_25 x1_5 x2
-	cd "$SRC"/$1
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1/${0%.svg}.png" -w 32 -h 32 $0' {} \;
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1_25/${0%.svg}.png" -w 40 -w 40 $0' {} \;
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1_5/${0%.svg}.png" -w 48 -w 48 $0' {} \;
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x2/${0%.svg}.png" -w 64 -w 64 $0' {} \;
 
-	cd $SRC
+    original_size=32
+    scales=(1 1.25 1.5 2)
+    svgs=$(ls -1 $1/*.svg)
+    total_images=$(echo "$svgs" | wc -l)
 
-	# generate cursors
-	BUILD="$SRC"/../dist
-	OUTPUT="$BUILD"/cursors
-	ALIASES="$SRC"/cursorList
+    rm -f $BUILD/config/*.cursor
+    for scale in ${scales[@]}; do
+        mkdir -p "$BUILD/$scale"
+    done
 
-	if [ ! -d "$BUILD" ]; then
-		mkdir "$BUILD"
-	fi
-	if [ ! -d "$OUTPUT" ]; then
-		mkdir "$OUTPUT"
-	fi
+    image_number=0
+    for svg in $svgs; do
+        let 'image_number+=1'
+        echo -en "\\rRendering PNGs $image_number/$total_images... "
+    
+        frame=$(get_frame $svg)
+        cursor=$(get_cursor $frame)
+        dst="$frame.png"
+        hotpoints_line=$(grep "^$cursor " "$HOTPOINTS")
+        hotx=$(echo "$hotpoints_line" | awk '{print $2}')
+        hoty=$(echo "$hotpoints_line" | awk '{print $3}')
+        delay_ms=$(echo "$hotpoints_line" | awk '{print $4}')
 
-	echo -ne "Generating cursor theme...\\r"
-	for CUR in config/*.cursor; do
-		BASENAME="$CUR"
-		BASENAME="${BASENAME##*/}"
-		BASENAME="${BASENAME%.*}"
-		
-		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
-	done
-	echo -e "Generating cursor theme... DONE"
+        for scale in ${scales[@]}; do
+            scale_name=$(echo "x$scale" | tr '.' '_')
+
+            size_px=$(echo "$original_size * $scale" | bc -l | sed -r 's/\.0+$//g')
+            inkscape $inkscape_opts "$BUILD/$scale_name/$dst" -w $size_px -h $size_px "$SRC/$svg" > /dev/null
+
+            hotx_scaled=$(echo "$hotx * $scale" | bc -l | sed 's/\..*$//g')
+            hoty_scaled=$(echo "$hoty * $scale" | bc -l | sed 's/\..*$//g')
+            config_line="$size_px $hotx_scaled $hoty_scaled $BUILD/$scale_name/$dst"
+            if [ "$delay_ms" ]; then
+                config_line+=" $delay_ms"
+            fi
+            echo "$config_line" >> "$BUILD/config/$cursor.cursor"
+        done
+    done
+    echo "DONE"
+
+    echo -n "Generating cursor theme... "
+    for svg in $svgs; do
+        cursor=$(get_cursor $(get_frame $svg))
+        xcursorgen "$BUILD/config/$cursor.cursor" "$OUTPUT/$cursor"
+    done
+    echo "DONE"
 
 	cd "$OUTPUT"	
 
 	#generate aliases
-	echo -ne "Generating shortcuts...\\r"
+	echo -n "Generating shortcuts... "
 	while read ALIAS; do
 		FROM="${ALIAS#* }"
 		TO="${ALIAS% *}"
@@ -46,17 +98,17 @@ function create {
 		fi
 		ln -sr "$FROM" "$TO"
 	done < "$ALIASES"
-	echo -e "Generating shortcuts... DONE"
+	echo "DONE"
 
 	cd "$PWD"
 
-	echo -ne "Generating Theme Index...\\r"
+	echo -n "Generating Theme Index... "
 	INDEX="$OUTPUT/../index.theme"
 	if [ ! -e "$OUTPUT/../$INDEX" ]; then
 		touch "$INDEX"
 		echo -e "[Icon Theme]\nName=$THEME\n" > "$INDEX"
 	fi
-	echo -e "Generating Theme Index... DONE"
+	echo "DONE"
 }
 
 # generate pixmaps from svg source
